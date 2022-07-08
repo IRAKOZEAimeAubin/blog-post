40c6e374a0e093483c51b61cfd173d237afabc94
Irakoze Aime Aubin <irakozeaimeaubin@gmail.com>
2022-07-08 12:30:58 +0200

    adding essentials for heroku hosting

--- app.js
+++ app.js
@@ -32,7 +32,9 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
-
+app.get('/', (req, res) => {
+    res.send('blog api')
+})
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/posts', authenticateUser, postsRouter)
